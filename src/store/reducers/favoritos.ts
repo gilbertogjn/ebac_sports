234ca684b0344bcac97c0d2a3b926ea530a843f7
Gilberto Garcia <gilbertogjn@gmail.com>
2024-01-17 16:30:40 -0400

    fix redux favoritos

---
+++ src/store/reducers/favoritos.ts
@@ -0,0 +1,33 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type FavoritosState = {
+  itens: Produto[]
+}
+
+const initialState: FavoritosState = {
+  itens: []
+}
+
+const favoritosSlice = createSlice({
+  name: 'favoritos',
+  initialState,
+  reducers: {
+    favoritar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      if (state.itens.find((p) => p.id === produto.id)) {
+        const favoritosSemProduto = state.itens.filter(
+          (p) => p.id !== produto.id
+        )
+        console.log(state.itens.push(...favoritosSemProduto))
+        state.itens = favoritosSemProduto
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { favoritar } = favoritosSlice.actions
+export default favoritosSlice.reducer
